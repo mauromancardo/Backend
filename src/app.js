2ca5332100f07c03163824c7195507ec26d833ae
mauromancardo <mauromancardo@gmail.com>
2023-09-20 17:01:50 -0300

    tpEntrega

--- src/app.js
+++ src/app.js
@@ -1,87 +1,14 @@
-import Express from "express";
-import { ProductManagerFiles } from "./persitence/files/productManageerFiles.js";
-const port = 8080;
-const app = Express();
+import express from "express";
+
 import { productsRouter } from "./routes/products.routes.js";
 import { cartsRouter } from "./routes/carts.routes.js";
 
-app.listen(port, () => console.log("servidor funcionando"));
+const port = 8080;
+const app = express();
 
+app.listen(port, () => console.log(`server running in port ${port}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//routes
-app.use("/api/products",productsRouter);
-app.use("/api/carts",cartsRouter);
-
-const managerProductService = new ProductManagerFiles(
-  "./src/files/productoss.json"
-);
-
-console.log(managerProductService);
-
-app.get("/products", async (req, res) => {
-  try {
-    await managerProductService.createProduct({
-      nombre: "coca cocla",
-      description: "bebida",
-      precio: "700",
-      img: "./imagen/coca.png",
-      code: "1",
-      stock: "5",
-    });
-    await managerProductService.createProduct({
-      nombre: "fanta",
-      description: "bebida",
-      precio: "800",
-      img: "./imagen/coca.png",
-      code: "2",
-      stock: "5",
-    });
-    await managerProductService.createProduct({
-      nombre: "sprite",
-      description: "bebida",
-      precio: "900",
-      img: "./imagen/coca.png",
-      code: "3",
-      stock: "5",
-    });
-    await managerProductService.createProduct({
-      nombre: "naranpol",
-      description: "bebida",
-      precio: "100",
-      img: "./imagen/coca.png",
-      code: "4",
-      stock: "5",
-    });
-
-    const products = await managerProductService.getProduct();
-    /* res.send(products); */
-const {limit} = req.query;
-const limitNumber = parseInt(limit);
-if (limit){
-    const productLimit = products.slice(0, limitNumber);
-    res.send(productLimit);
-}
-else{
-    res.send(products)
-}
-  } catch (error) {
-    res.send(error.message);
-  }
-});
-
-app.get("/products/:productId", async (req, res) => {
-  try {
-    const { productId } = req.params;
-    const parsedProductId = parseInt(productId);
-    const product = await managerProductService.getProductById(parsedProductId);
-    if (product) {
-      res.send(product);
-    } else {
-      res.send("id no encontrado");
-    }
-  } catch (error) {
-    res.send(error.nessage);
-  }
-});
-
-
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
